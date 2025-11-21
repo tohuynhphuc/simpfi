b9fe5545281512e6ddb6478ee77ad74cafeb03ee
Khainh2005 <10423053@student.vgu.edu.vn>
2025-11-22 04:02:46 +0700

    javadoc for Constants.java and Button.java

--- src/main/java/com/simpfi/config/Constants.java
+++ src/main/java/com/simpfi/config/Constants.java
@@ -1,5 +1,7 @@
 package com.simpfi.config;
-
+/** 
+ * Defines fixed values for file paths and drawing properties to ensure consistency when plotting maps and running programs.
+*/
 public class Constants {
 
 	/**
