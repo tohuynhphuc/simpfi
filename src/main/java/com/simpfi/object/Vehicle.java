6f27be0d12ba5948dafaaca025ab312c1babbaf7
Quanninh <quanninh05@gmail.com>
2025-11-22 00:01:12 +0100

    coding in Vehicle.java

--- src/main/java/com/simpfi/object/Vehicle.java
+++ src/main/java/com/simpfi/object/Vehicle.java
@@ -3,5 +3,45 @@ package com.simpfi.object;
 public class Vehicle {
 
 	private String id;
+	private double x;
+	private double y;
+	private double speed;
+	private String roadID;
+	private String type;
+	private double angle;
 
+	public Vehicle(String id, double x, double y, double speed, String roadID, String type, double angle){
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+		this.roadID = roadID;
+		this.type = type;
+		this.angle = angle;
+	}
+
+	public String getID(){
+		return this.id;
+	}
+
+	public double[] getPosition(){
+		double[] coordinate = {this.x, this.y};
+		return coordinate;
+	}
+
+	public double getSpeed(){
+		return this.speed;
+	}
+
+	public String getRoadID(){
+		return this.roadID;
+	}
+
+	public double getAngle(){
+		return this.angle;
+	}
+
+	public String getType(){
+		return this.type;
+	}
 }
