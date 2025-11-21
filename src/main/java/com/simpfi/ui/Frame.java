39937bb45f69f4d9c0c220a39a03a322d567c7f2
Khainh2005 <10423053@student.vgu.edu.vn>
2025-11-22 04:50:15 +0700

    adding basic javadoc for Frame.java & Panel.Java

--- src/main/java/com/simpfi/ui/Frame.java
+++ src/main/java/com/simpfi/ui/Frame.java
@@ -3,14 +3,17 @@ package com.simpfi.ui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
 
 import javax.swing.JFrame;
-
+/**
+ * Custom Frame class that inherits <@link javax.swing.JFrame>.
+ */
 public class Frame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-
+	/**
+	 * Constructor customed to match our software's style and place the frame in the center.
+	 */
 	public Frame() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(1120, 660);
@@ -24,12 +27,18 @@ public class Frame extends JFrame {
 
 		setVisible(true);
 	}
+	// /**
+	//  * Prints out the text in the clicked button (Phuc & Khanh check this).
+	//  * @param evt refers to the action, in this case 
+	//  */
+	// public void Test_Button(ActionEvent evt) {
+	// 	System.out.println(evt.getActionCommand());
+	// }
+	// /**
+	//  * The method is intended to output information details to users
+	//  * @param evt same as above
+	//  */
+	// public void ShowInformation(ActionEvent evt) {
 
-	public void Test_Button(ActionEvent evt) {
-		System.out.println(evt.getActionCommand());
-	}
-
-	public void ShowInformation(ActionEvent evt) {
-
-	}
+	// }
 }
