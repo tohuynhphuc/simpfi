7537dc89c6dfe133f4ccce85646771ee954e8f05
Khainh2005 <10423053@student.vgu.edu.vn>
2025-11-22 23:58:17 +0700

    javadoc for XMLReader.java

--- src/main/java/com/simpfi/util/XMLReader.java
+++ src/main/java/com/simpfi/util/XMLReader.java
@@ -1,24 +1,35 @@
 package com.simpfi.util;
 
 import java.io.File;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Document;
 
+import org.w3c.dom.Document;
+/**
+ * Creates XMLReader used to read and parse XML files.
+ */
 public class XMLReader {
 
 	protected File file;
 	private DocumentBuilderFactory factory;
 	private DocumentBuilder builder;
 	protected Document document;
-
+	/**
+	 * Constructor used to initialize and parse XML files.
+	 * @param fileAddress directory of the XML file
+	 */
 	public XMLReader(String fileAddress) throws Exception {
 		file = new File(fileAddress);
 		factory = DocumentBuilderFactory.newInstance();
 		builder = factory.newDocumentBuilder();
 		document = builder.parse(file);
 	}
-
+	/**
+	 * Used to convert a String of multiple pairs of coordinates to an array of Point.
+	 * @param shape the String that contains pairs of coordinates with the format: "x,y u,t a,b ...".
+	 * @return Array of Points.
+	 */
 	protected Point[] extractPoints(String shape) {
 		String[] points = shape.split(" ");
 		Point[] pointArr = new Point[points.length];
