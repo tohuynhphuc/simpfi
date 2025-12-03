63f239cc0b679ab7f16c3020319a0e99f1ce3460
Khainh2005 <10423053@student.vgu.edu.vn>
2025-12-04 02:19:56 +0700

    create and add javadoc to CheckBox.java

--- src/main/java/com/simpfi/ui/CheckBox.java
+++ src/main/java/com/simpfi/ui/CheckBox.java
@@ -12,6 +12,7 @@ import com.simpfi.config.Constants;
  */
 public class CheckBox extends JCheckBox {
 
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
 	// public CheckBox() {
@@ -24,20 +25,39 @@ public class CheckBox extends JCheckBox {
 	// 	init();
 	// }
 
+	/**
+	 * Constructor used to create a check box with a given text and a checked state.
+	 * 
+	 * @param text the text goes with the check box
+	 * @param selected boolean value whether the box is ticked or not
+	 */
 	public CheckBox(String text, boolean selected) {
 		super(text, selected);
 		init();
 	}
 
+	/**Some settings for the checkboxes to appear more attractively on the user interface.*/
 	private void init() {
 		this.setFont(Constants.DEFAULT_FONT);
         this.setFocusable(false);
 	}
 
+
+	/**
+	 * Method used to set the state of the check box.
+	 * 
+	 * @param checked the state of the check box
+	 */
 	public void setChecked(boolean checked) {
 		this.setSelected(checked);
 	}
 
+
+	/**
+	 * Returns the state of the check box.
+	 *
+	 * @return {@code true} if the check box is selected; {@code false} otherwise
+	 */
 	public boolean isChecked() {
 		return this.isSelected();
 	}
