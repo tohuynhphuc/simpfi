93231f7994ebb689da6ebd2f5c1a02ab61fd5195
Khainh2005 <10423053@student.vgu.edu.vn>
2025-12-06 05:56:43 +0700

    create Scroll Pane to visualize large lists of data (lists of edges)

---
+++ src/main/java/com/simpfi/ui/ScrollPane.java
@@ -0,0 +1,37 @@
+package com.simpfi.ui;
+
+import javax.swing.BoxLayout;
+import javax.swing.JScrollPane;
+
+public class ScrollPane extends JScrollPane{
+
+    /** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+
+    private Panel contentPanel;
+
+    public ScrollPane(){
+        // Create inner panel
+        contentPanel = new Panel();
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+        //contentPanel.setPreferredSize(new Dimension(200, 300));
+        
+        // Set content panel as viewport
+        setViewportView(contentPanel);
+
+        init();
+    }
+
+    private void init(){
+        getVerticalScrollBar().setUnitIncrement(16);
+        setFocusable(false);
+    }
+
+    public void addCheckBoxLists(CheckBox[] options){
+        for(CheckBox i : options){
+            contentPanel.add(i);
+        }
+        contentPanel.revalidate();
+        contentPanel.repaint();
+    }
+}
